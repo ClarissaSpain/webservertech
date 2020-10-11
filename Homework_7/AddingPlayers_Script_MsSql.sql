5dbb4eee976e92803d3db0eef6e1aa38e615ec60
Clarissa Spain <35749695+ClarissaSpain@users.noreply.github.com>
2020-10-10 20:26:50 -0600

    Homework 7

---
+++ Homework_7/AddingPlayers_Script_MsSql.sql
@@ -0,0 +1,3 @@
+SELECT * FROM Players ORDER BY firstName;
+
+--INSERT INTO Players(userID, firstName, lastName, emailAddress, Phone, username, gamertag) VALUES('2', 'Jane', 'smith', 'smith46@outlook.com', '406-999-9990', 'smithers', 'smithing');
---
+++ Homework_7/Insert_select_script_mysql.sql
@@ -0,0 +1,4 @@
+SELECT * FROM players
+
+-- INSERT INTO players (userID, firstName, lastName, emailAddress, phone, gamertag, username) VALUES ('0', 'Claire', 'Spain', 'skook6770@gmail.com', '4068804981', 'Clairesweet', 'Clairesweet');
+-- INSERT INTO players (userID, firstName, lastName, emailAddress, phone, gamertag, username) VALUES ('1', 'Colton', 'Gerth', 'colton11@gmail.com', '4068390504', 'Gerthygerth', 'ColtonG');
